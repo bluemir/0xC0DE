1104a65fe912dea2088b59774fc5f1924ab5d5ad
BlueMir <bluemir@bluemir.me>
2023-11-08 21:21:39 +0900

    Add resets & fix cert gen script

--- scripts/makefile.d/certs.mk
+++ scripts/makefile.d/certs.mk
@@ -1,8 +1,21 @@
 ##@ Cert
 
-certs: runtime/certs/server-cert.yaml ## Generate self signed certs
+certs: ## Generate self signed certs
+
+certs: runtime/certs/server.crt runtime/certs/server.bundle.crt
+
+# customize SAN via OPTIONAL_SAN
+# eg.
+# runtime/certs/server.crt: export OPTIONAL_SAN=",DNS:dev.0xC0DE.io"
+
+
+cert-secrets: ## make k8s secret file
+cert-secrets: runtime/deploy/server.bundle.yaml
+
+##########################################################################
 
 .PRECIOUS: runtime/certs/%.key runtime/certs/%.csr runtime/certs/%.crt
+.PRECIOUS: runtime/certs/%.bundle.crt
 
 runtime/certs/ca.key:
 	@mkdir -p $(dir $@)
@@ -37,19 +50,21 @@ runtime/certs/%.crt: runtime/certs/%.csr runtime/certs/ca.crt runtime/certs/ca.k
 	# check issuer: openssl x509 -subject -issuer -noout -in $@
 	# check SAN:    openssl x509 -text -noout -in $@ | grep "Subject Alternative Name" -A1
 	################################################################################
+runtime/certs/%.bundle.crt: runtime/certs/%.crt runtime/certs/ca.crt
+	cat $^ > $@
 
 # customize SAN via OPTIONAL_SAN
 # eg.
 # runtime/certs/server.crt: export OPTIONAL_SAN=",DNS:dev.0xC0DE.io"
 
-runtime/certs/ca.yaml: runtime/certs/ca.crt
+runtime/deploy/ca.yaml: runtime/certs/ca.crt
 	@mkdir -p $(dir $@)
 	kubectl create secret generic $(APP_NAME)-ca \
 		--from-file=$< \
 		--dry-run -o yaml \
 		> $@
 
-runtime/certs/%-cert.yaml: runtime/certs/%.crt runtime/certs/%.key runtime/certs/ca.crt
+runtime/deploy/%-cert.yaml: runtime/certs/%.crt runtime/certs/%.key runtime/certs/ca.crt
 	@mkdir -p $(dir $@)
 	kubectl create secret generic $(APP_NAME)-$*-cert \
 		--from-file=tls.crt=runtime/certs/$*.crt \
