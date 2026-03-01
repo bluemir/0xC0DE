9f7d9dba3a1027e2d5d4f6a4d62450d6d9db29ec
BlueMir <bluemir@bluemir.me>
2026-03-01 16:56:38 +0900

    fix: support macOS zip format in etcd install script
    Use zip extraction on darwin and tar.gz on other platforms to match etcd's release artifacts.

--- scripts/tools/install/etcd.sh
+++ scripts/tools/install/etcd.sh
@@ -8,12 +8,21 @@ BIN_DIR=$(pwd)/${2:-runtime/tools}
 initArch
 initOS
 
+case $OS in
+  darwin) EXT="zip";;
+  *)      EXT="tar.gz";;
+esac
+
 TMP_DIR=$(mktemp -d)
 trap 'rm -rf "$TMP_DIR"' EXIT
 cd $TMP_DIR
 
-curl -LO "https://github.com/etcd-io/etcd/releases/download/$TAG/etcd-$TAG-$OS-$ARCH.tar.gz"
-tar -xvf "etcd-$TAG-$OS-$ARCH.tar.gz"
+curl -LO "https://github.com/etcd-io/etcd/releases/download/$TAG/etcd-$TAG-$OS-$ARCH.$EXT"
+
+case $EXT in
+  zip)    unzip "etcd-$TAG-$OS-$ARCH.$EXT";;
+  tar.gz) tar -xvf "etcd-$TAG-$OS-$ARCH.$EXT";;
+esac
 
 mkdir -p $BIN_DIR
 mv etcd-$TAG-$OS-$ARCH/etcd etcd-$TAG-$OS-$ARCH/etcdctl $BIN_DIR
